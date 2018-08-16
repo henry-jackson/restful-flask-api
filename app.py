9428276fcf0fc82466aeb0152653c40c54e75f97
Henry Jackson <henry.jackson.95@gmail.com>
2018-08-16 15:11:06 -0400

    adds User model and config file

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
+import config
 import models
 from resources.courses import courses_api
 from resources.reviews import reviews_api
 
-DEBUG = True
-HOST = '0.0.0.0'
-PORT = 8000
+
 
 app = Flask(__name__)
 app.register_blueprint(courses_api, url_prefix='/api/v1')
@@ -20,4 +19,4 @@ def hello_world():
 
 if __name__ == '__main__':
     models.initialize()
-    app.run(debug=DEBUG, host=HOST, port=PORT)
+    app.run(debug=config.DEBUG, host=config.HOST, port=config.PORT)
